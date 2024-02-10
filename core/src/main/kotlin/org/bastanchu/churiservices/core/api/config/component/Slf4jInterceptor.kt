fe61c5d3accba702eab2c295b04eb5b045e4803e
angel <vellebue@gmail.com>
2024-02-10 17:43:56 +0100

    feat: Added support for Correlation id and Transaction id on headers.
    Correlation Id and Transaction Id are configured for all microservices. They are also visible on HTTP response header attributes.

--- core/src/main/kotlin/org/bastanchu/churiservices/core/api/config/component/Slf4jInterceptor.kt
+++ core/src/main/kotlin/org/bastanchu/churiservices/core/api/config/component/Slf4jInterceptor.kt
@@ -4,22 +4,30 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.MDC
 import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.ModelAndView
 import java.lang.Exception
 import java.util.UUID
 
 class Slf4jInterceptor : HandlerInterceptor {
 
-    val correlationIdHeader = "X-Correlation-ID"
+    val correlationIdHeader = "X-CORRELATION-ID"
+    val transactionIdHeader = "X-TRANSACTION-ID"
     val correlationIdLogAttribute = "correlationId"
+    val transactionIdLogAttribute = "transactionId"
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         val correlationId = getOrGenerateCorrelationId(request)
+        val transactionId = getTransactionId()
+        response.setHeader(correlationIdHeader, correlationId)
+        response.setHeader(transactionIdHeader, transactionId)
         MDC.put(correlationIdLogAttribute, correlationId)
+        MDC.put(transactionIdLogAttribute, transactionId)
         return true
     }
 
     override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
         MDC.remove(correlationIdLogAttribute)
+        MDC.remove(transactionIdLogAttribute)
     }
 
     private fun getOrGenerateCorrelationId(request: HttpServletRequest) : String {
@@ -29,5 +37,9 @@ class Slf4jInterceptor : HandlerInterceptor {
         } else {
             return correlationIdFromHeader.toString()
         }
+    }
+
+    private fun getTransactionId() : String {
+        return UUID.randomUUID().toString();
     }
 }
